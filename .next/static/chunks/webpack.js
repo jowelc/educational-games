969e590003400d5f62015fa676e490d51b06d84e
Jowel Castenada <jowelcastaneda6@gmail.com>
2024-08-19 21:42:19 +0800

    fix errors

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0f179f3c79f487ce"; }
+/******/ 		__webpack_require__.h = function() { return "212a3edb2897f883"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- layouts/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { Outlet } from 'umi'
-
-export default function Layout() {
-  return <Outlet />
-}
